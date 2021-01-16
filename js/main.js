603d69aa266cdc88ce8f5d836e82469e5e8bdb3e
Jacob Flora <62020858+Jacob52210@users.noreply.github.com>
2021-01-15 21:47:36 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+const resultsEl = document.getElementById('results');
+let resultsHTML = '';
+
+searchForm.addEventListener('submit', function(e) {
+    e.preventDefault()
+    const q = searchInput.value;
+    search(q)
+})
+
+function search(q) {
+    const apiKey = "9KemuYKuM7DozeEmiGoMHyPT9qqCBgdO"
+    const path = `http://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${q}`
+
+ //---------------------Error Handling------------------------------//
+   
+
+ //---------------Fetching and Converting Data----------------------//
+    fetch(path).then(function(res) {  
+       return res.json()
+    }).then(function(json) {
+       // console.log(json.data[0].images.fixed_width.url)
+        
+        
+
+        json.data.forEach(function(obj) {
+            //console.log(obj);
+
+            const url =  obj.images.fixed_width.url;
+            const alt = obj.title;
+
+            resultsHTML += `<img src="${url}"  
+                             alt="${alt}"
+                             width="auto"
+                             height="100px"
+                             >`
+        })
+
+        resultsEl.innerHTML = resultsHTML
+    }).catch(function(err) {
+        console.log(err.message)
+        
+    });
+}
