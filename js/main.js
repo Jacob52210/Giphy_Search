698840d257d41207431c9aa49b0d8d7ad2b86c6e
Jacob Flora <jacobflora4@gmail.com>
2021-01-15 22:07:29 -0500

    changed http to https

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ searchForm.addEventListener('submit', function(e) {
 
 function search(q) {
     const apiKey = "9KemuYKuM7DozeEmiGoMHyPT9qqCBgdO"
-    const path = `http://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${q}`
+    const path = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${q}`
 
  //---------------------Error Handling------------------------------//
    
